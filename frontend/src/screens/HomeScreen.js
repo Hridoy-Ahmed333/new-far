81a5bbb9c2c47141726abc74f0014fab8930698b
Hridoy-Ahmed333 <hridoyahmedf5@gmail.com>
2023-05-26 23:27:49 +0600

    product screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -42,7 +42,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Amazona</title>
+        <title>The Comfort Zone</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
